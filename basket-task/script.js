96bcf8bcded6aa4733f106d7ea5fab94eb6c5e64
unknown <rashadmkh@code.edu.az>
2021-12-08 19:03:25 +0400

    Update 3.0

--- basket-task/script.js
+++ basket-task/script.js
@@ -14,6 +14,7 @@ BasketProductCount();
 addBtns.forEach((item) => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
+
     let Id = this.parentElement.parentElement.getAttribute("data-id");
     let Price = this.previousElementSibling.innerHTML;
     let Title = this.parentElement.firstElementChild.innerHTML;
@@ -33,6 +34,7 @@ addBtns.forEach((item) => {
       existProduct.count++;
     }
     console.log(basket);
+
     localStorage.setItem("basket", JSON.stringify(basket));
     BasketProductCount();
   });
@@ -41,41 +43,60 @@ function getBasket(Id, price, title, image) {
   let basket = JSON.parse(localStorage.getItem("basket"));
 }
 function createrow() {
-    let tbody = document.querySelector(".tbody");
+  let tbody = document.querySelector(".tbody");
   let basket = JSON.parse(localStorage.getItem("basket"));
-  basket.forEach(item =>{
+  basket.forEach((item) => {
     let newtd1 = document.createElement("td");
     newtd1.innerHTML = item.id;
+    newtd1.setAttribute("style", "padding-top :25px");
     let newtd2 = document.createElement("td");
-    newtd2.innerHTML=item.title;
+    newtd2.innerHTML = item.title;
+    newtd2.setAttribute("style", "padding-top :25px");
     let newtd3 = document.createElement("td");
-    newtd3.innerHTML=item.price;
+    newtd3.innerHTML = item.price;
+    newtd3.setAttribute("style", "padding-top :25px");
     let newtd4 = document.createElement("td");
-    let newtd4img = document.createElement("img")
-    newtd4.appendChild(newtd4img)
-    newtd4img.setAttribute("src",item.image)
+    let newtd4img = document.createElement("img");
+    newtd4.appendChild(newtd4img);
+    newtd4img.setAttribute("src", item.image);
+    newtd4img.style.width = "50px";
     let newtd5 = document.createElement("td");
-    newtd5.innerHTML=item.count;
-    let newtd6 = document.createElement("td")
-    let td6btn = document.createElement("button")
-    td6btn.setAttribute("class","btn btn-danger")
+    newtd5.innerHTML = item.count;
+    newtd5.setAttribute("style", "padding-top :25px");
+    let newtd6 = document.createElement("td");
+    let td6btn = document.createElement("button");
+    td6btn.setAttribute("class", "btn btn-danger");
     td6btn.innerHTML = "Delete";
+    td6btn.setAttribute("style", "margin-top :10px");
     newtd6.appendChild(td6btn);
-    td6btn.addEventListener("click",function (e){
-      let table = document.querySelector(".table")
-      console.log(table);
-      tbody.deleteRow(this)
-
-    })
     let newtr = document.createElement("tr");
-    newtr.appendChild(newtd1)
-    newtr.appendChild(newtd2)
-    newtr.appendChild(newtd3)
-    newtr.appendChild(newtd4)
-    newtr.appendChild(newtd5)
-    newtr.appendChild(newtd6)     
-    tbody.appendChild(newtr)
-  })
+    newtr.setAttribute("id", newtd1.innerHTML);
+    newtr.appendChild(newtd1);
+    newtr.appendChild(newtd4);
+    newtr.appendChild(newtd2);
+    newtr.appendChild(newtd3);
+    newtr.appendChild(newtd5);
+    newtr.appendChild(newtd6);
+    tbody.appendChild(newtr);
+  });
 }
 createrow();
 
+let deletebtns = document.querySelectorAll(".btn-danger");
+deletebtns.forEach((item,idx) => {
+
+  item.addEventListener("click", function (e) {
+   
+    let table = document.querySelector(".table");
+    item.closest("tr").remove();
+    let basket = JSON.parse(localStorage.getItem("basket"));
+
+ if(basket.filter(x=>x.id == item.parentElement.parentElement.id)){
+
+     basket.splice(0,1);
+     localStorage.setItem("basket", JSON.stringify(basket));
+     BasketProductCount();
+   
+ }
+  });
+});
